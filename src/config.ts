fe0fba66d640c130208b2742e70ccda8e3225c95
glglak <karem.kamel@gmail.com>
2025-06-26 05:22:41 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://logs-of-a-thinking-machine.vercel.app/", // replace with your actual domain
+  author: "Karim",
+  profile: "https://linkedin.com/in/moodraz",
+  desc: "Where architecture meets anxiety, abstraction, and automation.",
+  title: "Logs of a Thinking Machine",
+  ogImage: "logs-of-a-thinking-machine-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 100, // remove pagination
+  postPerPage: 100,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showArchives: false,
+  showBackButton: false,
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "",
   },
-  dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  dynamicOgImage: false,
+  dir: "ltr",
+  lang: "en",
+  timezone: "Asia/Riyadh",
 } as const;
